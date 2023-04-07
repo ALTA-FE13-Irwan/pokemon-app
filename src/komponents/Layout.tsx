60c6c7755cf4a2636a8979834c4d17633a5ce373
IrwanFicoFar <irwanhadi9@gmail.com>
2023-04-08 05:35:30 +0700

    finish also with ui and reponsive

--- src/komponents/Layout.tsx
+++ src/komponents/Layout.tsx
@@ -1,5 +1,17 @@
-import { FC } from "react";
+import { FC, ReactNode } from "react";
 
-export const Layout: FC = () => {
-  return <div>Layout</div>;
+import { Navbar } from "./Navbar";
+
+interface Props {
+  children: ReactNode;
+}
+
+export const Layout: FC<Props> = (props) => {
+  const { children } = props;
+  return (
+    <div className="bg-gradient-to-b from-sky-950 via to-sky-900">
+      <Navbar />
+      <div className=" p-5 md:p-10">{children}</div>;
+    </div>
+  );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,13 @@
 import { FC, useState, useEffect } from "react";
-import axios from "axios";
-import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
+import { Layout } from "../komponents/Layout";
+import Swal from "sweetalert2";
+import axios from "axios";
 
-interface Pokemon {
-  id: number;
-  name: string;
-  height: number;
-  weight: number;
-  types: {
-    type: {
-      name: string;
-    };
-  }[];
-  abilities: {
-    ability: {
-      name: string;
-    };
-  }[];
-  moves: {
-    move: {
-      name: string;
-    };
-  }[];
-  sprites: {
-    front_default: string;
-  };
-}
+import { Pokemon } from "../utils/user";
 
 const Home: FC = () => {
   const [pokemonData, setPokemonData] = useState<Pokemon[]>([]);
-  const [pokemonImage, setPokemonImage] = useState<string>(``);
   const [loading, setLoading] = useState<boolean>(true);
 
   const navigate = useNavigate();
@@ -69,38 +46,37 @@ const Home: FC = () => {
   }
 
   return (
-    <div>
-      <h1>Home Page</h1>
-      <div className="grid gap-5 grid-cols-2 md:grid-cols-4 xl:grid-cols-6 ">
-        {pokemonData.map((pokemon) => (
-          <div className="bg-slate-400 text-center items-center flex flex-col ">
-            <div key={pokemon.id} className="flex align-middle">
-              <img
-                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
-                alt={pokemon.name}
-              />
-            </div>
-            <div>
-              <div>{pokemon.name}</div>
-              <div>
-                <strong>Types:</strong>{" "}
-                {pokemon.types.map((type) => type.type.name).join(", ")}
+    <Layout>
+      <div>
+        <div className="grid gap-5 md:gap-10 grid-cols-2 md:grid-cols-3 xl:grid-cols-5">
+          {pokemonData.map((pokemon) => (
+            <div className=" text-center items-center  bg-sky-600 hover:bg-sky-300 bg-opacity-20 rounded-t-full rounded-b-lg hover:scale-105">
+              <div
+                key={pokemon.id}
+                className="flex flex-col align-middle w-full aspect-square"
+              >
+                <img
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
+                  alt={pokemon.name}
+                  className="w-full aspect-square"
+                />
+                <div className="text-sky-50 font-semibold py-5 text-2xl md:text-3xl capitalize">
+                  {pokemon.name}
+                </div>
+
+                <button
+                  type="button"
+                  className=" antialiased py-3 md:py-5 px-4 inline-flex justify-center items-center font-bold bg-sky-500 text-sky-950 hover:bg-sky-600 transition-all text-lg md:text-2xl capitalize"
+                  onClick={() => navigate(`/${pokemon.id}`)}
+                >
+                  detail
+                </button>
               </div>
             </div>
-            <div>
-              <button
-                type="button"
-                className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
-                onClick={() => navigate(`/${pokemon.id}`)}
-              >
-                detail
-              </button>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-      <button onClick={() => navigate("/list")}>go to lsit pokemon</button>
-    </div>
+    </Layout>
   );
 };
 
