db2d2eca2db3a8daccf6978659032f957f1c0b47
IrwanFicoFar <irwanhadi9@gmail.com>
2023-04-08 13:36:26 +0700

    use loading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,33 +48,37 @@ const Home: FC = () => {
   return (
     <Layout>
       <div>
-        <div className="grid gap-5 md:gap-10 grid-cols-2 md:grid-cols-3 xl:grid-cols-5">
-          {pokemonData.map((pokemon) => (
-            <div className=" text-center items-center  bg-sky-600 hover:bg-sky-300 bg-opacity-20 rounded-t-full rounded-b-lg hover:scale-105">
-              <div
-                key={pokemon.id}
-                className="flex flex-col align-middle w-full aspect-square"
-              >
-                <img
-                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
-                  alt={pokemon.name}
-                  className="w-full aspect-square"
-                />
-                <div className="text-sky-50 font-semibold py-5 text-2xl md:text-3xl capitalize">
-                  {pokemon.name}
-                </div>
-
-                <button
-                  type="button"
-                  className=" antialiased py-3 md:py-5 px-4 inline-flex justify-center items-center font-bold bg-sky-500 text-sky-950 hover:bg-sky-600 transition-all text-lg md:text-2xl capitalize"
-                  onClick={() => navigate(`/${pokemon.id}`)}
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          <div className="grid gap-5 md:gap-10 grid-cols-2 md:grid-cols-3 xl:grid-cols-5">
+            {pokemonData.map((pokemon) => (
+              <div className=" text-center items-center  bg-sky-600 hover:bg-sky-300 bg-opacity-20 rounded-t-full rounded-b-lg hover:scale-105">
+                <div
+                  key={pokemon.id}
+                  className="flex flex-col align-middle w-full aspect-square"
                 >
-                  detail
-                </button>
+                  <img
+                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
+                    alt={pokemon.name}
+                    className="w-full aspect-square"
+                  />
+                  <div className="text-sky-50 font-semibold py-5 text-2xl md:text-3xl capitalize">
+                    {pokemon.name}
+                  </div>
+
+                  <button
+                    type="button"
+                    className=" antialiased py-3 md:py-5 px-4 inline-flex justify-center items-center font-bold bg-sky-500 text-sky-950 hover:bg-sky-600 transition-all text-lg md:text-2xl capitalize"
+                    onClick={() => navigate(`/${pokemon.id}`)}
+                  >
+                    detail
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        )}
       </div>
     </Layout>
   );
